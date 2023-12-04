f6fbd945aee3c0a0276f490f8ce65d5c9c889cd7
juniorb34 <juniorb34fortal@gmail.com>
2023-12-04 12:40:06 -0300

    finalizar aplicação

--- client_tcp.py
+++ client_tcp.py
@@ -1,4 +1,12 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(client)
+client.settimeout(60)
+
+try:
+
+    client.connect(("127.0.0.1", 6000))
+    client.send(b'TEXTO\n')
+    pacotes_recebidos = client.recv(100000).decode()
+except:
+    print("Ocorreu um erro!")
